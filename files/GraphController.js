7dd3e5d972ea24f572e4c0a0c2ebc7921a399666
Jacques Menuet <jmenuet@gmail.com>
2016-12-17 17:00:16 +0100

    Little fixes and re-ordering the functions

--- files/GraphController.js
+++ files/GraphController.js
@@ -51,255 +51,6 @@ GraphController.prototype.update = function()
 	GraphDataPresenter.update( this._canvas, this._graphData, this._graphDataWindow, this._graphOptions );
 };
 
-GraphController.prototype.zoom = function( zoomFactor, graphWindowPoint, axes )
-{
-	if ( !graphWindowPoint )
-		graphWindowPoint = {x:0.5, y:0.5};
-	
-	var graphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphWindowPoint, this._graphDataWindow );
-
-	if ( !axes )
-		axes = 'xy';
-
-	if ( axes.indexOf('x')!==-1 )
-		this._graphDataWindow.width *= zoomFactor;
-
-	if ( axes.indexOf('y')!==-1 )
-		this._graphDataWindow.height *= zoomFactor;
-
-	var graphDataPoint2 = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphWindowPoint, this._graphDataWindow );
-
-	this._graphDataWindow.x -= (graphDataPoint2.x - graphDataPoint.x);
-	this._graphDataWindow.y -= (graphDataPoint2.y - graphDataPoint.y);
-
-	if ( this._onGraphDataWindowChange )
-	{
-		this._onGraphDataWindowChange();
-	}
-};
-
-GraphController._getCanvasPointFromEvent = function( event )
-{
-	// The mouse position in the event happening on the canvas is relative to the whole document,
-	// not the canvas. The "offset" properties is what we want, but it's not consistantly available 
-	// on all browsers, so we calculate it here. See http://www.jacklmoore.com/notes/mouse-position/
-	var target = event.target || event.srcElement;
-	var rect = target.getBoundingClientRect();
-	var offsetX = event.clientX - rect.left;
-	var offsetY = event.clientY - rect.top;
-	return {x:offsetX, y:offsetY};
-};
-
-GraphController.prototype._onKeyDown = function( event )
-{
-	var c0 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:0, y:0}, this._graphDataWindow );
-	var c1 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:1, y:1}, this._graphDataWindow );
-	var s = 0.1;
-	var dx = (c1.x - c0.x) * s;
-	var dy = (c1.y - c0.y) * s;
-
-	var windowChanged = false;
-	if ( event.keyCode===37 )				// left arrow
-	{
-		this._graphDataWindow.x -= dx;
-		windowChanged = true;
-	}
-	else if ( event.keyCode===39 )			// right arrow
-	{
-		this._graphDataWindow.x += dx;
-		windowChanged = true;
-	}
-	else if ( event.keyCode===38 )			// up arrow
-	{
-		this._graphDataWindow.y += dy;
-		windowChanged = true;
-	}
-	else if ( event.keyCode===40 )			// down arrow
-	{
-		this._graphDataWindow.y -= dy;
-		windowChanged = true;
-	}
-	else if ( event.keyCode===187 ||  event.keyCode===189 )		// +/= and -/_
-	{
-		var zoomFactor = 1;
-		var k = 0.1;
-		if ( event.keyCode===187 )
-		{	
-			zoomFactor -= k;
-		}
-		else if ( event.keyCode===189 )
-		{
-			zoomFactor += k;
-		}
-		this.zoom( zoomFactor );
-		this.windowChanged = true;
-	}
-	
-	if ( this.windowChanged )
-	{
-		this.update();
-		if ( this._onGraphDataWindowChange )
-		{
-			this._onGraphDataWindowChange();
-		}
-	}
-};
-
-GraphController.prototype._onMouseDown = function( event )
-{
-	this._lastCanvasPoint = GraphController._getCanvasPointFromEvent( event );
-};
-
-GraphController.prototype._onMouseMove = function( event )
-{
-	if ( !this._lastCanvasPoint )
-		return;
-
-	var canvasPoint = GraphController._getCanvasPointFromEvent( event );
-	var graphDataPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( canvasPoint, this._canvas );
-	graphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphDataPoint, this._graphDataWindow );
-
-	var lastGraphDataPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( this._lastCanvasPoint, this._canvas );
-	lastGraphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( lastGraphDataPoint, this._graphDataWindow );
-
-	var deltaX = graphDataPoint.x - lastGraphDataPoint.x;
-	var deltaY = graphDataPoint.y - lastGraphDataPoint.y;
-	this._graphDataWindow.x -= deltaX;
-	this._graphDataWindow.y -= deltaY;
-
-	this._lastCanvasPoint = canvasPoint;
-	this._lastCanvasPointsFromTouches = null;
-
-	this.update();
-	if ( this._onGraphDataWindowChange )
-	{
-		this._onGraphDataWindowChange();
-	}
-};
-
-GraphController.prototype._onMouseUp = function( event )
-{
-	this._lastCanvasPoint = null;
-};
-
-GraphController.prototype._onWheel = function( event )
-{
-	if ( event.ctrlKey )
-	{
-		var wheelDelta = 0;
-		if ( Math.abs(event.deltaX)>Math.abs(event.deltaY) )
-		{
-			wheelDelta = event.deltaX;
-		}
-		else
-		{
-			wheelDelta = event.deltaY;
-		}
-
-		var canvasPoint = GraphController._getCanvasPointFromEvent( event );
-		var	graphWindowPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( canvasPoint, this._canvas );
-		
-		var zoomFactor = 1.0;
-		var k = 0.001
-		if ( wheelDelta>0 )
-		{	
-			zoomFactor += k;
-		}
-		else
-		{
-			zoomFactor -= k;
-		}
-
-		var axes = 'x';
-		if ( event.shiftKey )
-		{
-			axes = 'y';
-		}
-		
-		for ( var i=0; i<Math.abs(wheelDelta); ++i )
-		{
-			this.zoom( zoomFactor, graphWindowPoint, axes );
-		}
-	}
-	else
-	{
-		var c0 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:0, y:0}, this._graphDataWindow );
-		var c1 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:1, y:1}, this._graphDataWindow );
-		var s = 0.001;					// Decent factor for OSX, might differ for other platform/browser 
-		var dx = event.deltaX * (c1.x - c0.x) * s;
-		var dy = event.deltaY * (c1.y - c0.y) * s;
-		this._graphDataWindow.x += dx;	// Proper sign for OSX, might differ for other platform/browser 
-		this._graphDataWindow.y -= dy;
-	}
-
-	this.update();
-		
-	if ( this._onGraphDataWindowChange )
-	{
-		this._onGraphDataWindowChange();
-	}
-
-	event.preventDefault();
-};
-
-
-GraphController._getTouchesFromEvent = function( event )
-{
-	var target = event.target || event.srcElement;
-	var rect = target.getBoundingClientRect();
-	var touches = {};
-	for ( var i=0; i<event.changedTouches.length; i++ )
-	{
-		var touch = event.changedTouches[i];
-		var id = touch.identifier;
-		var offsetX = touch.clientX - rect.left;
-		var offsetY = touch.clientY - rect.top;
-		touches[id] = {x:offsetX, y:offsetY, id:id};
-	}
-	return touches;
-};
-
-GraphController._getTouchIndexById = function( touches, touchId )
-{
-	for ( var i=0; i<touches.length; ++i )
-	{
-		if ( touches[i].id.toString()===touchId.toString() )
-			return i;
-	}
-	return -1;
-};
-
-GraphController._cloneTouches = function( touches )
-{
-	var clonedTouches = [];
-	for ( var i=0; i<touches.length; ++i )
-	{
-		clonedTouches.push( {
-			id: touches[i].id,
-			x: touches[i].x,
-			y: touches[i].y
-		});
-	}
-	return clonedTouches;
-};
-
-GraphController.prototype._onTouchStart = function( event )
-{
-	var canvasPoints = GraphController._getTouchesFromEvent( event );
-	for ( var id in canvasPoints )
-	{
-		var index = GraphController._getTouchIndexById( this._touches, id );
-		if ( index!==-1 )
-		{
-			delete this._touches[index];	// If somehow we already had this touch in our array (this means we've missed a touch end), we removed it
-		}
-
-		this._touches.push( canvasPoints[id] );
-	}
-
-	event.preventDefault();		// Preventing default on touch events prevent the "pull to refresh" feature on Chrome Android
-};
-
 // Update the graph data window to zoom by the given factors on x and y axes while preserving the graph data point
 // at the given canvas point to that same canvas position
 GraphController.prototype._zoom = function( canvasPoint, zoomFactorX, zoomFactorY )
@@ -345,6 +96,215 @@ GraphController.prototype._panAndZoom = function( firstCanvasPointFrom, firstCan
 		zy = (ptB0.y - ptA0.y) / (ptB1.y - ptA0.y);
 
 	this._zoom( firstCanvasPointFrom, zx, zy );
+};
+
+///!!!
+GraphController.prototype.zoom = function( zoomFactor, graphWindowPoint, axes )
+{
+	if ( !graphWindowPoint )
+		graphWindowPoint = {x:0.5, y:0.5};
+	
+	var graphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphWindowPoint, this._graphDataWindow );
+
+	if ( !axes )
+		axes = 'xy';
+
+	if ( axes.indexOf('x')!==-1 )
+		this._graphDataWindow.width *= zoomFactor;
+
+	if ( axes.indexOf('y')!==-1 )
+		this._graphDataWindow.height *= zoomFactor;
+
+	var graphDataPoint2 = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphWindowPoint, this._graphDataWindow );
+
+	this._graphDataWindow.x -= (graphDataPoint2.x - graphDataPoint.x);
+	this._graphDataWindow.y -= (graphDataPoint2.y - graphDataPoint.y);
+
+	if ( this._onGraphDataWindowChange )
+	{
+		this._onGraphDataWindowChange();
+	}
+};
+
+GraphController.prototype._onKeyDown = function( event )
+{
+	var c0 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:0, y:0}, this._graphDataWindow );
+	var c1 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:1, y:1}, this._graphDataWindow );
+	var s = 0.1;
+	var dx = (c1.x - c0.x) * s;
+	var dy = (c1.y - c0.y) * s;
+
+	var windowChanged = false;
+	if ( event.keyCode===37 )				// left arrow
+	{
+		this._graphDataWindow.x -= dx;
+		windowChanged = true;
+	}
+	else if ( event.keyCode===39 )			// right arrow
+	{
+		this._graphDataWindow.x += dx;
+		windowChanged = true;
+	}
+	else if ( event.keyCode===38 )			// up arrow
+	{
+		this._graphDataWindow.y += dy;
+		windowChanged = true;
+	}
+	else if ( event.keyCode===40 )			// down arrow
+	{
+		this._graphDataWindow.y -= dy;
+		windowChanged = true;
+	}
+	else if ( event.keyCode===187 ||  event.keyCode===189 )		// +/= and -/_
+	{
+		var zoomFactor = 1;
+		var k = 0.1;
+		if ( event.keyCode===187 )
+		{	
+			zoomFactor -= k;
+		}
+		else if ( event.keyCode===189 )
+		{
+			zoomFactor += k;
+		}
+		this.zoom( zoomFactor );
+		windowChanged = true;
+	}
+	
+	if ( windowChanged )
+	{
+		this.update();
+		if ( this._onGraphDataWindowChange )
+		{
+			this._onGraphDataWindowChange();
+		}
+	}
+};
+
+GraphController.prototype._onMouseDown = function( event )
+{
+	this._lastCanvasPoint = GraphController._getCanvasPointFromMouseEvent( event );
+};
+
+GraphController.prototype._onMouseMove = function( event )
+{
+	if ( !this._lastCanvasPoint )
+		return;
+
+	var canvasPoint = GraphController._getCanvasPointFromMouseEvent( event );
+	var graphDataPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( canvasPoint, this._canvas );
+	graphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( graphDataPoint, this._graphDataWindow );
+
+	var lastGraphDataPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( this._lastCanvasPoint, this._canvas );
+	lastGraphDataPoint = GraphDataPresenter.graphWindowPointToGraphDataPoint( lastGraphDataPoint, this._graphDataWindow );
+
+	var deltaX = graphDataPoint.x - lastGraphDataPoint.x;
+	var deltaY = graphDataPoint.y - lastGraphDataPoint.y;
+	this._graphDataWindow.x -= deltaX;
+	this._graphDataWindow.y -= deltaY;
+
+	this._lastCanvasPoint = canvasPoint;
+	this._lastCanvasPointsFromTouches = null;
+
+	this.update();
+	if ( this._onGraphDataWindowChange )
+	{
+		this._onGraphDataWindowChange();
+	}
+};
+
+GraphController.prototype._onMouseUp = function( event )
+{
+	this._lastCanvasPoint = null;
+};
+
+GraphController.prototype._onWheel = function( event )
+{
+	if ( event.ctrlKey )
+	{
+		var wheelDelta = 0;
+		if ( Math.abs(event.deltaX)>Math.abs(event.deltaY) )
+		{
+			wheelDelta = event.deltaX;
+		}
+		else
+		{
+			wheelDelta = event.deltaY;
+		}
+
+		var canvasPoint = GraphController._getCanvasPointFromMouseEvent( event );
+		var	graphWindowPoint = GraphDataPresenter.canvasPointToGraphWindowPoint( canvasPoint, this._canvas );
+		
+		var zoomFactor = 1.0;
+		var k = 0.001
+		if ( wheelDelta>0 )
+		{	
+			zoomFactor += k;
+		}
+		else
+		{
+			zoomFactor -= k;
+		}
+
+		var axes = 'x';
+		if ( event.shiftKey )
+		{
+			axes = 'y';
+		}
+		
+		for ( var i=0; i<Math.abs(wheelDelta); ++i )
+		{
+			this.zoom( zoomFactor, graphWindowPoint, axes );
+		}
+	}
+	else
+	{
+		var c0 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:0, y:0}, this._graphDataWindow );
+		var c1 = GraphDataPresenter.graphWindowPointToGraphDataPoint( {x:1, y:1}, this._graphDataWindow );
+		var s = 0.001;					// Decent factor for OSX, might differ for other platform/browser 
+		var dx = event.deltaX * (c1.x - c0.x) * s;
+		var dy = event.deltaY * (c1.y - c0.y) * s;
+		this._graphDataWindow.x += dx;	// Proper sign for OSX, might differ for other platform/browser 
+		this._graphDataWindow.y -= dy;
+	}
+
+	this.update();
+		
+	if ( this._onGraphDataWindowChange )
+	{
+		this._onGraphDataWindowChange();
+	}
+
+	event.preventDefault();
+};
+
+GraphController._getCanvasPointFromMouseEvent = function( event )
+{
+	// The mouse position in the event happening on the canvas is relative to the whole document,
+	// not the canvas. The "offset" properties is what we want, but it's not consistantly available 
+	// on all browsers, so we calculate it here. See http://www.jacklmoore.com/notes/mouse-position/
+	var target = event.target || event.srcElement;
+	var rect = target.getBoundingClientRect();
+	var offsetX = event.clientX - rect.left;
+	var offsetY = event.clientY - rect.top;
+	return {x:offsetX, y:offsetY};
+};
+
+GraphController.prototype._onTouchStart = function( event )
+{
+	var canvasPoints = GraphController._getTouchesFromEvent( event );
+	for ( var id in canvasPoints )
+	{
+		var index = GraphController._getTouchIndexById( this._touches, id );
+		if ( index!==-1 )
+		{
+			delete this._touches[index];	// If somehow we already had this touch in our array (this means we've missed a touch end), we removed it
+		}
+
+		this._touches.push( canvasPoints[id] );
+	}
+
+	event.preventDefault();		// Preventing default on touch events prevent the "pull to refresh" feature on Chrome Android
 };
 
 GraphController.prototype._onTouchMove = function( event )
@@ -393,3 +353,44 @@ GraphController.prototype._onTouchEnd = function( event )
 
 	event.preventDefault();
 };
+
+GraphController._getTouchesFromEvent = function( event )
+{
+	var target = event.target || event.srcElement;
+	var rect = target.getBoundingClientRect();
+	var touches = {};
+	for ( var i=0; i<event.changedTouches.length; i++ )
+	{
+		var touch = event.changedTouches[i];
+		var id = touch.identifier;
+		var offsetX = touch.clientX - rect.left;
+		var offsetY = touch.clientY - rect.top;
+		touches[id] = {x:offsetX, y:offsetY, id:id};
+	}
+	return touches;
+};
+
+GraphController._cloneTouches = function( touches )
+{
+	var clonedTouches = [];
+	for ( var i=0; i<touches.length; ++i )
+	{
+		clonedTouches.push( {
+			id: touches[i].id,
+			x: touches[i].x,
+			y: touches[i].y
+		});
+	}
+	return clonedTouches;
+};
+
+GraphController._getTouchIndexById = function( touches, touchId )
+{
+	for ( var i=0; i<touches.length; ++i )
+	{
+		if ( touches[i].id.toString()===touchId.toString() )
+			return i;
+	}
+	return -1;
+};
+
